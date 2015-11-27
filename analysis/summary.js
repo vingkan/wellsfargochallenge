eef36d761645f4c83ec701646c31ef68de435645
Vinesh Kannan <vingkan@gmail.com>
2015-11-27 13:00:12 -0600

    Actually analyze all given datapoints

--- analysis/summary.js
+++ analysis/summary.js
@@ -1,7 +1,7 @@
 function getWordMap(dataset, thisWordMap){
 	var wordMap = thisWordMap || new WordMap();
 	var size = dataset.length;
-	for(var d = 0; d < 10; d++){
+	for(var d = 0; d < size; d++){
 		var words = dataset[d].text.split(" ");
 		var numOfWords = words.length;
 		//console.log(words);
