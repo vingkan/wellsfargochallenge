3966ca1f04f1018572cd42deefc8aec35599ec3d
Vinesh Kannan <vingkan@gmail.com>
2015-11-29 14:30:42 -0600

    Refactoring

--- analysis/main.js
+++ analysis/main.js
@@ -1,9 +1,13 @@
 //var testData = {json data initialized in data/stringified.js};
 //var wordMap = new WordMap();
-
 //var testData;
+
+var storage = new Storage();
+
 var IGNORE_THESE_WORDS = ['to', 'the', 'in', 'of', 'is', '&', 'a', 'it', 'i', 'who', 'you', 'your', 'and', 'will', 'for', 'be', 'with', 'they', 'we', 'are', 'on', 'at', 'what', 'me', 'too', 'in', 'for', 'an', 'for', 'their', 'when', 'its', 'my', 'from', 'have', 'had', 'this', 'or', 'if', 'was', 'by', 'has', 'as', 'do', 'would', 'dont', 'there', 'oh', 'didnt', 'wasnt', 'were', 'should', 'used', 'rt'];
 var baselineWordMap = new WordMap(IGNORE_THESE_WORDS);
+
+getRandomDataArray(storage, 100, 'sample', testDataAnalysis);
 
 function testDataAnalysis(dataset){
 	//WORD MAP ANALYSIS
@@ -32,8 +36,5 @@ function testDataAnalysis(dataset){
 	frequencyMap.getResults(3, false);
 	frequencyMap.getResults(3, true);
 }
-
-var storage = new Storage();
-getRandomDataArray(storage, 100, 'sample', testDataAnalysis);
 
 console.log("LOADED main.js");
